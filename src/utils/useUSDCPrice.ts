bebf34aedfc2144e0d98b33e2527027b12d4b44f
Daniel <daniel0ar@hotmail.com>
2024-06-12 10:16:28 -0500

    refactor: Remove unused hook file

--- src/utils/useUSDCPrice.ts
+++
@@ -1,7 +0,0 @@
-import { USDC } from '@/constants'
-import { ChainId, NativeCurrency, Price } from '@monadex/sdk'
-
-export default function useUSDCPrice (currency?: NativeCurrency): Price | undefined {
-  if (currency == null) { return undefined }
-  return new Price(currency, USDC[ChainId.MONAD], BigInt(1), BigInt(1))
-}
